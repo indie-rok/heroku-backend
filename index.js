a5eac91f5cd798929024a7276794c77e39132213
Emmanuel Orozco <indi3.rok@gmail.com>
2022-01-17 16:05:55 +0100

    added heroku rules

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 express()
+  .use(cors())
   .get("/", (req, res) => res.send({ message: "Hello World!" }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
